f0c69ebb48a02fe34ec8fb3d7678897796f7002f
pcorbel <pierre@polaranalytics.co>
2024-06-10 22:04:45 +0200

    fix: :bug: fix parasyte

--- app/auto.sh
+++ app/auto.sh
@@ -7,6 +7,6 @@ export PYTHONPATH=${PARASYTE_PATH}/python2.7:${PARASYTE_PATH}/python2.7/site-pac
 export PYTHONHOME=${PARASYTE_PATH}/python2.7:${PARASYTE_PATH}/python2.7/site-packages:${PARASYTE_PATH}/python2.7/lib-dynload
 export LD_LIBRARY_PATH=${PARASYTE_PATH}:${PARASYTE_PATH}/python2.7/:${PARASYTE_PATH}/python2.7/lib-dynload:$LD_LIBRARY_PATH
 
-eval /mnt/SDCARD/.userdata/miyoomini/parasyte/bin/python /mnt/SDCARD/.userdata/miyoomini/app.py  # >> /mnt/SDCARD/.userdata/miyoomini/hello-miyoo.log 2>&1
+eval /mnt/SDCARD/.userdata/miyoomini/parasyte/bin/python /mnt/SDCARD/.userdata/miyoomini/app.py >> /mnt/SDCARD/.userdata/miyoomini/logs/hello-miyoo.log 2>&1
 
 unset LD_PRELOAD
